5a4e7eb22568dcd36b0fc4df6210c4f417e74026
Jack Bodman <jack.bodman1998@outlook.com>
2020-08-29 18:04:18 +0100

    Movies Error
    Fixed error in movies migration file

--- CinemaSystem/database/migrations/2020_08_27_125748_create_movies_table.php
+++ CinemaSystem/database/migrations/2020_08_27_125748_create_movies_table.php
@@ -15,7 +15,7 @@ class CreateMoviesTable extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
-            $table->name();
+            $table->string('name');
             $table->timestamps();
         });
     }
