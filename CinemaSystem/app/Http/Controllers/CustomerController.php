3a00125b2efc621128031bbf5c11e9ca4df50c03
Jack Bodman <jack.bodman1998@outlook.com>
2020-08-30 22:57:04 +0100

    Customer Create
    Customer Create function added

--- CinemaSystem/app/Http/Controllers/CustomerController.php
+++ CinemaSystem/app/Http/Controllers/CustomerController.php
@@ -3,12 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+//Models
+use App\Customer;
 
 class CustomerController extends Controller
 {
     //Create Customer
     public function create(Request $request) {
-        return 'Customer Create Called';
+        try {
+            
+            //Validate
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|email|unique:customers',
+                'name' => 'required'
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    'error' => $validator->errors()
+                ])->setStatusCode(400);
+            }
+
+            //Map to Model and Save in DB
+            $customer = new Customer;
+            $customer->email = $request->input('email');
+            $customer->name = $request->input('name');
+            $customer->save();
+
+            //Return Success Message
+            return response()->json([
+                'Message' => 'Customer Saved'
+            ])->setStatusCode(200);
+
+
+        } catch (Throwable $error) {
+            //Return Error Message
+            return response()->json([
+                'error' => $error 
+            ])->setStatusCode(500);
+
+        }
     }
 
     //Update Customer
