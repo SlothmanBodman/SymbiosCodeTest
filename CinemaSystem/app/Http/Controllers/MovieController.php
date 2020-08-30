ecd8d510f5fcd3d1b4a59a830c9ab7f32e75e6c4
Jack Bodman <jack.bodman1998@outlook.com>
2020-08-31 00:38:06 +0100

    Movie Create
    Create Movie method added

--- CinemaSystem/app/Http/Controllers/MovieController.php
+++ CinemaSystem/app/Http/Controllers/MovieController.php
@@ -3,12 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+//Models
+use App\Movie;
 
 class MovieController extends Controller
 {
     //Create Movie
     public function create(Request $request) {
-        return 'Movie Create Called';
+        try {
+            //Validate
+            $validator = Validator::make($request->all(), [
+                'name' => 'required'
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    'error' => $validator->errors()
+                ])->setStatusCode(400);
+            }
+
+            //Map to Model and Save in DB
+            $movie = new Movie;
+            $movie->name = $request->input('name');
+            $movie->save();
+
+            //Return Success Message
+            return response()->json([
+                'Message' => 'Movie Saved'
+            ])->setStatusCode(200);
+            
+        } catch (Throwable $error) {
+            //Return Error Message
+            return response()->json([
+                'error' => $error 
+            ])->setStatusCode(500);
+
+        }
     }
 
     //Update Movie
