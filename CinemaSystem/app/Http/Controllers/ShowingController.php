e014906bca8dba8cf7a684297b0bc2ca3322236c
Jack Bodman <jack.bodman1998@outlook.com>
2020-08-31 18:02:47 +0100

    Showing Update
    Updated Showing Method Added

--- CinemaSystem/app/Http/Controllers/ShowingController.php
+++ CinemaSystem/app/Http/Controllers/ShowingController.php
@@ -66,7 +66,61 @@ class ShowingController extends Controller
     
     //Update Showing
     public function update(Request $request) {
-        return 'Showing Update Called';
+        try {
+
+            //Find Existing Showing
+            $showing = Showing::find($request->input('id'));
+            if ($showing == null) {
+                return response()->json([
+                    'error' => 'Showing Not Found'
+                ])->setStatusCode(404);
+            }
+
+            //Validate Request
+            $validator = Validator::make($request->all(), [
+                'movie_id' => 'required',
+                'screen_id' => 'required',
+                'start_time' => 'required|date'
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    'error' => $validator->errors()
+                ])->setStatusCode(400);
+            }
+
+            //Check Related Data Exists
+            //Movie
+            $movie = Movie::find($request->input('movie_id'));
+            if ($movie == null) {
+                return response()->json([
+                    'error' => 'Movie Not Found'
+                ])->setStatusCode(404);
+            }
+            //Customer
+            $screen = Screen::find($request->input('screen_id'));
+            if ($screen == null) {
+                return response()->json([
+                    'error' => 'Screen Not Found'
+                ])->setStatusCode(404);
+            }
+
+            //Update and Save Showing
+            $showing->movie_id = $request->input('movie_id');
+            $showing->screen_id = $request->input('screen_id');
+            $showing->start_time = $request->input('start_time');
+            $showing->save();
+
+            //Return Success Message
+            return response()->json([
+                'Message' => 'Showing Updated'
+            ])->setStatusCode(200);
+
+        } catch (Throwable $error) {
+            //Return Error Message
+            return response()->json([
+                'error' => $error 
+            ])->setStatusCode(500); 
+        }
     }
     
     //Delete Showing
