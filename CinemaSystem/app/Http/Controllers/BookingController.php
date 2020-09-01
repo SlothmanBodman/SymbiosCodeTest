c4686bc3870b8c77cd3f3e5b2b2923d7a525aa1d
Jack Bodman <jack.bodman1998@outlook.com>
2020-09-01 22:57:04 +0100

    Create Booking
    Added create booking method

--- CinemaSystem/app/Http/Controllers/BookingController.php
+++ CinemaSystem/app/Http/Controllers/BookingController.php
@@ -3,44 +3,94 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 use App\Customer;
 use App\Showing;
 Use App\BookedSeat;
 Use App\Booking;
+Use App\Seat;
 
 class BookingController extends Controller
 {
         //Create Booking
         public function create(Request $request) {
-                //Check Customer Exists
-                $customer = Customer::find($request->input('customer_id'));
-                if ($customer == null) {
-                        return response()->json([
-                                'error' => 'Customer Not Found'
-                        ])->setStatusCode(404);                       
-                }
 
-                //Check Showing Exists
-                $showing = Showing::find($request->input('showing_id'));
-                if ($showing == null) {
-                        return response()->json([
-                                'error' => 'Showing Not Found'
-                        ])->setStatusCode(404);   
-                }
-                //Check Seat Availability
-                $requestedSeats = $request->input('requested_seats');
-                foreach($requestedSeats as $requestedSeat) {
-                        
-                }
+                try {
+                        //Validate Request Data
+                        $validator = Validator::make($request->all(), [
+                                'customer_id' => 'required',
+                                'showing_id' => 'required',
+                                'requested_seats' => 'required'
+                        ]);
+                        if ($validator->fails()) {
+                                return response()->json([
+                                    'error' => $validator->errors()
+                                ])->setStatusCode(400);
+                        }
 
-                //Create and Save Booking 
+                        //Check Customer Exists
+                        $customer = Customer::find($request->input('customer_id'));
+                        if ($customer == null) {
+                                return response()->json([
+                                        'error' => 'Customer Not Found'
+                                ])->setStatusCode(404);                       
+                        }
 
+                        //Check Showing Exists
+                        $showing = Showing::find($request->input('showing_id'));
+                        if ($showing == null) {
+                                return response()->json([
+                                        'error' => 'Showing Not Found'
+                                ])->setStatusCode(404);   
+                        }
+                        //Check Seat Availability and If Exists
+                        $requestedSeats = $request->input('requested_seats');
+                        foreach($requestedSeats as $requestedSeat) {
+                                //Check Seat Exists
+                                $seatInDb = Seat::find($requestedSeat);
+                                if($seatInDb == null) {
+                                        return response()->json([
+                                               'error' => 'Seat Not Found'
+                                       ])->setStatusCode(404); 
+                               }
+                               //Check Not Booked
+                                $doubleBookedSeat = BookedSeat::where('showing_id', $request->input('showing_id'))
+                               ->where('seat_id', $requestedSeat)->first();
+                               if ($doubleBookedSeat != null) {
+                                        return response()->json([
+                                                'error' => 'One of the Selected Seats has Been Booked'
+                                        ])->setStatusCode(404);  
+                              } 
+                       
+                        }
 
-                //Create and Save Seats
+                        //Create and Save Booking
+                        $booking = new Booking;
+                        $booking->customer_id = $request->input('customer_id'); 
+                        $booking->showing_id = $request->input('showing_id');
+                        $booking->save();
+
+                        //Create and Save Seats
+                        foreach($requestedSeats as $bookedSeat) {
+                                $seat = new BookedSeat;
+                                $seat->booking_id = $booking->id;
+                                $seat->seat_id = $bookedSeat;
+                                $seat->showing_id = $request->input('showing_id');
+                                $seat->save(); 
+                        }
                 
-                //Return Response
+                        //Return Response
+                        return response()->json([
+                        'Message' => 'Booking Saved'
+                        ])->setStatusCode(200); 
 
+                } catch (Throwable $error) {
+                        //Return Error Message
+                        return reponse()->json([
+                                'error' => $error
+                        ])->setStatusCode(500);   
+                }
         }
     
         //Update Booking
